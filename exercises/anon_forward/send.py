379e1c74a05f7112d6f5f169b1383a9c654a3941
Ruhang Ma <m.ruhang@berkeley.edu>
2023-05-08 00:55:25 -0500

    anonFoward implemented & untested

---
+++ exercises/anon_forward/send.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import argparse
+import random
+import socket
+
+from myTunnel_header import MyTunnel
+from scapy.all import IP, TCP, Ether, get_if_hwaddr, get_if_list, sendp
+
+
+def get_if():
+    ifs=get_if_list()
+    iface=None # "h1-eth0"
+    for i in get_if_list():
+        if "eth0" in i:
+            iface=i
+            break;
+    if not iface:
+        print("Cannot find eth0 interface")
+        exit(1)
+    return iface
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ip_addr', type=str, help="The destination IP address to use")
+    parser.add_argument('message', type=str, help="The message to include in packet")
+    parser.add_argument('--anon', type=int, default=None, help='Specify any integer to activate anon forward, if unspecified then packets will be forwarded with normal behavior.')
+    args = parser.parse_args()
+
+    addr = socket.gethostbyname(args.ip_addr)
+    anon = args.anon
+    iface = get_if()
+
+    if (anon is not None):
+        print("sending with anonymity {}".format(iface))
+        pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
+        pkt = pkt / IP(dst=addr) / TCP(dport=1234, sport=5000) / args.message
+    else:
+        print("sending with anonymity {}".format(iface))
+        pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
+        pkt = pkt / IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / args.message
+
+    pkt.show2()
+#    hexdump(pkt)
+#    print "len(pkt) = ", len(pkt)
+    sendp(pkt, iface=iface, verbose=False)
+
+
+if __name__ == '__main__':
+    main()
